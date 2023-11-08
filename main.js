55ee902e12b483bc0d22005360cbdeaec7cd0dec
Poloflv <kevinpolo316@gmail.com>
2023-11-07 23:13:25 -0500

    local storage / persistencia del modo oscuro

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const changeThemeHTML = document.querySelector("#changeTheme");
 
-changeThemeHTML.addEventListener("click", function () {
-    document.body.classList.toggle("darkmode")
+const isDark = () => JSON.parse(localStorage.getItem("isDark"));
+document.body.classList.toggle("darkmode", isDark());
+
+changeThemeHTML.addEventListener("click", () => {
+    if (isDark()) {
+        localStorage.setItem("isDark", JSON.stringify(false));
+        document.body.classList.remove("darkmode");
+    } else {
+        localStorage.setItem("isDark", JSON.stringify(true));
+        document.body.classList.add("darkmode");
+
+    }
 
 });
