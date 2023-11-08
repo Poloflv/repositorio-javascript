b56d1cb58a14842ae7f6c7c9e70a274857d447c8
Poloflv <kevinpolo316@gmail.com>
2023-11-08 15:42:39 -0500

    cambios en aprendizaje

--- script.js
+++ script.js
@@ -18,24 +18,24 @@ function seleccionar() {
 }
 // animaciones de las habilidades
 
-function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300 ){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("bubble");
-        habilidades[3].classList.add("flutter");
-        habilidades[4].classList.add("react");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("motivacion");
-    }
-}
+// function efectoHabilidades() {
+//     var skills = document.getElementById("skills");
+//     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+//     if(distancia_skills >= 300 ){
+//         let habilidades = document.getElementsByClassName("progreso");
+//         habilidades[0].className.add("javascript");
+//         habilidades[1].classList.add("htmlcss");
+//         habilidades[2].classList.add("bubble");
+//         habilidades[3].classList.add("flutter");
+//         habilidades[4].classList.add("react");
+//         habilidades[5].classList.add("comunicacion");
+//         habilidades[6].classList.add("trabajo");
+//         habilidades[7].classList.add("creatividad");
+//         habilidades[8].classList.add("dedicacion");
+//         habilidades[9].classList.add("motivacion");
+//     }
+// }
 
-window.onscroll = function() {
-    efectoHabilidades();
-} 
+// window.onscroll = function() {
+//     efectoHabilidades();
+// } 
